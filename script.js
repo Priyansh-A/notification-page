fd6944a139532f7850a9ba6a69feaaaf31986de5
Priyanshu <121610106+Priyansh-A@users.noreply.github.com>
2023-02-18 13:52:38 +0545

    fimish

---
+++ script.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector(".mark");
+let notifications = document.querySelector("#notifications");
+let state = document.querySelectorAll(".state")
+let select = document.querySelectorAll(".card");
+for(let i = 0; i<3; i++){
+        let action = state[i];
+        let read = select[i];
+        btn.addEventListener("click", ()=>{
+            action.classList.remove("active");
+            read.classList.remove("unread")
+            notifications.innerHTML = "0" ;
+        })
+}
+let marked = document.getElementById("notifications")
+let numb = Number.parseInt(marked.textContent);
+for(let i = 0; i<3; i++){
+    let read = select[i];
+    let action = state[i];
+    read.addEventListener("click", ()=>{
+        read.classList.remove("unread")
+        action.classList.remove("active");
+        numb--;
+        marked.textContent = numb;
+    })
+}
